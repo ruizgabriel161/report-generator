b97730cfac59c4ae57db08b39e1c43ec10c416b5
ruizgabriel161 <ruizgabriel161@outlook.com>
2023-08-19 17:00:03 -0300

    alteração create_app.v.25

--- src/ext/admin_view.py
+++ src/ext/admin_view.py
@@ -38,4 +38,4 @@ def init_app(app):
     admin.name = app.config.TITLE
     admin.template_mode = "bootstrap3"
     admin.init_app(app)
-    admin.add_view(UserAdmin(model=User, session=db.session))
+    admin.add_view(UserAdmin(model=User, session=db.session, name="user_admin"))
