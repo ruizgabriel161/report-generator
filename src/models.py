d5b2d81fc3ce44ff8f3d63404eeb3abb5a60e56a
ruizgabriel161 <ruizgabriel161@outlook.com>
2023-08-19 17:48:31 -0300

    alteração create_app.v.26

--- src/models.py
+++ src/models.py
@@ -1,9 +1,8 @@
-from sqlalchemy_serializer import SerializerMixin
 
 from src.ext.database import db
 
 
-class User(db.Model, SerializerMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(140), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
