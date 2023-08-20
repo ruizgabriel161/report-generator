a82373e2cbf24a0898e183249faef02da960adff
ruizgabriel161 <ruizgabriel161@outlook.com>
2023-08-20 14:56:50 -0300

    alteração create_app.v.36

--- app.py
+++ app.py
@@ -13,5 +13,6 @@ def create_app(environ, start_response, **config):
 
     app = minimal_app(**config)
 
-    configuration.load_extensions(app)
+    socket_istance = configuration.load_extensions(app)
+    socket_istance.run(host='0.0.0.0', port=10000)
     return app(environ=environ, start_response=start_response)
